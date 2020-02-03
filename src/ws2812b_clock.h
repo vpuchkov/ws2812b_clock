3eb2736bccb45ca045ebaad44c77924a0984dd27
Владимир Пучков <v.puchkov@corp.rosintel>
2020-02-03 11:16:02 +0300

    123

--- src/ws2812b_clock.h
+++ src/ws2812b_clock.h
@@ -7,8 +7,6 @@
 
 //Работа с сетью
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-
-//needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
@@ -18,14 +16,6 @@
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 10800, 600000);
 
-/*
-  Скетч к проекту "Бегущая строка"
-  Страница проекта (схемы, описания): https://alexgyver.ru/GyverString/
-  Исходники на GitHub: https://github.com/AlexGyver/GyverString/
-  Нравится, как написан код? Поддержи автора! https://alexgyver.ru/support_alex/
-  Автор: AlexGyver, AlexGyver Technologies, 2019
-  https://AlexGyver.ru/
-*/
 
 // ================ НАСТРОЙКИ ================
 #define BRIGHTNESS 150         // стандартная яркость (0-255)
